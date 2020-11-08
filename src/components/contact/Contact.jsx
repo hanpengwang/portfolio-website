99ae4e6ad9bf85f1e1bff0728b164c8a2032f0f5
hanpeng <wanghanpeng95@gmail.com>
2020-11-07 22:55:34 -0400

    contact form update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,23 +2,59 @@ import React from "react";
 import "./Contact.css";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: { email: "", message: "" },
+    };
+    this.msgPostAPI = "https://jsonplaceholder.typicode.com/posts";
+  }
+
   render() {
     return (
       <div className="container-form">
-        <form
-          onSubmit={(e) => {
-            alert("sent msg");
-            e.preventDefault();
-          }}
-        >
-          <input type="text" placeholder="Email" />
-
-          <textarea id="subject" placeholder="Message goes here"></textarea>
-          <input type="submit" value="SEND" />
+        <form onSubmit={this.handleSend}>
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            autoComplete="email"
+            onChange={this.handleInput}
+          />
+          <textarea
+            name="message"
+            id="subject"
+            placeholder="Message goes here"
+            onChange={this.handleInput}
+          />
+          <input type="submit" value="SEND"></input>
+          <i class="fa fa-refresh fa-spin"></i>
         </form>
       </div>
     );
   }
+
+  handleSend = (e) => {
+    (async () => {
+      const rawResponse = await fetch(this.msgPostAPI, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.state.form),
+      });
+      const content = await rawResponse.json();
+    })();
+    e.preventDefault();
+  };
+  handleInput = (e) => {
+    const targetName = e.target.name;
+    const targetValue = e.target.value;
+    this.setState({
+      form: { ...this.state.form, [targetName]: targetValue },
+    });
+  };
 }
 
 export default Contact;
