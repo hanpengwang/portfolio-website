f9b14ada2460d888cbe0049c83ea4c3928d2606c
hanpeng <wanghanpeng95@gmail.com>
2020-11-07 22:54:33 -0400

    home page animation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,43 +1,188 @@
 import React from "react";
 import ContentContainer from "../content-container/ContentContainer";
+import Typist from "react-typist";
 import Keyboard from "./Keyboard";
 
+import "./Home.css";
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codeInputs: [],
+    };
+    this.codeInputRef = React.createRef();
+  }
+
+  handleCodeEnter = (e) => {
+    let codeValue = e.target.value;
+
+    if (e.key == "Enter") {
+      this.codeInputRef.current.placeholder = "";
+      this.setState({
+        codeInputs: [...this.state.codeInputs, codeValue],
+      });
+      e.target.value = "";
+      switch (codeValue) {
+        case "Hanpeng_Dev.get_contact()":
+          window.location.assign(
+            `${window.location.protocol}//${window.location.host}/#findme`
+          );
+          break;
+        case "Hanpeng_Dev.show_me_your_code()":
+          window.location.assign(
+            `${window.location.protocol}//${window.location.host}/projects`
+          );
+          break;
+        default:
+          this.codeInputRef.current.placeholder =
+            'try "Hanpeng_Dev.show_me_your_code()"';
+          break;
+      }
+    }
+  };
+
+  handleTypingDone = () => {
+    let inputEle = this.codeInputRef.current;
+    inputEle.focus();
+    this.inputTyping(inputEle);
+  };
+
+  inputTyping = (ele) => {
+    let index = 0;
+    let defaultCode = "Hanpeng_Dev.get_contact()";
+
+    (function typeWriter() {
+      if (index < defaultCode.length) {
+        ele.defaultValue += defaultCode.charAt(index);
+        index++;
+        let speed = Math.random() * 150;
+        setTimeout(typeWriter, speed);
+      }
+    })();
+  };
+
   render() {
     return (
       <div id="home">
         <ContentContainer>
-          <div>
-            <div style={textStyles}>
-              <h1>Hello World</h1>
-              <h2>It's Hanpeng Wang </h2>
-              <h3>I develop applications for interests and good</h3>
-            </div>
-            <a href="#aboutme" style={bottomButtonStyles}>
-              <button>know me more</button>
-            </a>
-          </div>
+          <Typist
+            cursor={{
+              show: false,
+            }}
+            avgTypingDelay={0}
+            stdTypingDelay={0}
+            onCharacterTyped={(char) => {}}
+            onTypingDone={this.handleTypingDone}
+          >
+            <div className="python-code">
+              <p>
+                <span className="python-class">Class </span>
+                <span>
+                  Hanpeng_Dev(
+                  <span className="python-inheritObject">object</span>
+                  ):
+                </span>
+              </p>
+              <p>
+                <span className="python-def">def </span>
+                <span>
+                  __init__(<span className="python-argSelf">self</span>):
+                </span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.last_name = </span>
+                <span>"Wang"</span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.first_name = </span>
+                <span>"Hanpeng"</span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.中文名 = </span>
+                <span>"王瀚鹏"</span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.yoe = </span>
+                <span>"3 years"</span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.fac_lang = </span>
+                <span>"Python"</span>
+              </p>
 
-          {/* <Keyboard /> */}
+              <p>
+                <span className="python-self">self</span>
+                <span>.cur_res = </span>
+                <span>"Canada, NS"</span>
+              </p>
+              <p>
+                <span className="python-self">self</span>
+                <span>.relacationn_pref = </span>
+                <span>"Anywhere in the world"</span>
+              </p>
+
+              <p className="python-staticmethod">@staticmethod</p>
+              <p>
+                <span className="python-def">def </span>
+                <span>get_contact():</span>
+              </p>
+              <p className="python-pass">pass</p>
+              <p className="python-staticmethod">@staticmethod</p>
+              <p>
+                <span className="python-def">def </span>
+                <span>show_me_your_code():</span>
+              </p>
+              <p className="python-pass">pass</p>
+            </div>
+          </Typist>
+
+          <>
+            {/* add input code */}
+            {this.state.codeInputs.map((element, i) => {
+              return (
+                <p className="python-input-code" key={i}>
+                  {element}
+                </p>
+              );
+            })}
+            <br />
+          </>
+          <input
+            type="text"
+            ref={this.codeInputRef}
+            onKeyDown={this.handleCodeEnter}
+          />
         </ContentContainer>
       </div>
     );
   }
 }
 
-const bottomButtonStyles = {
-  position: "absolute",
-  bottom: "3vh",
-  left: "calc(10vw + (90vw - 140px)/2)",
-  textAlign: "center",
-};
+const myIntro = `class Hanpeng_Dev(object):
 
-const textStyles = {
-  marginTop: "30vh",
-  marginLeft: "auto",
-  marginRight: "auto",
-  left: 0,
-  right: 0,
-  textAlign: "center",
-};
+def __init__(self):
+    self.last_name = "Wang"
+    self.first_name = "Hanpeng"
+    self.中文名 = "王瀚鹏"
+    self.yoe = "3 years"
+    self.fav_lang = "Python"
+    self.cur_res = "Canada, NS"
+    self.relocation_pref = "Anywhere in the World"
+    
+@staticmethod
+def get_contact():
+    pass
+
+@staticmethod
+def show_me_your_code():
+    pass
+
+`;
+
 export default Home;
