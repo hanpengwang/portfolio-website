edd54bfb80797745c9cb0437f5f285a40907f35d
hanpeng <wanghanpeng95@gmail.com>
2020-11-11 01:15:08 -0400

    set project db api

---
+++ src/components/projects/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Card, CardColumns, Container } from "react-bootstrap";
+
+export default function ProjectCard(props) {
+  const cardStyles = {
+    width: "400px",
+    height: "400px",
+    verticalAlign: "top",
+    display: "inline-block",
+    margin: "30px 10px 0 30px",
+    backgroundColor: "rgba(255, 0, 0, 0)",
+    boxShadow: "0px 0px 10px 1px rgba(224,224,224,1)",
+  };
+  return (
+    <Card style={cardStyles}>
+      <Card.Img src={props.media} style={{ width: "auto", height: "50%" }} />
+      <Card.Body>
+        <Card.Title>{"project " + props.title}</Card.Title>
+        <Card.Text>{props.text}</Card.Text>
+        <Card.Link href={props.link}>Get to know more</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,64 +1,51 @@
 import React from "react";
-import { Card, CardColumns, Container } from "react-bootstrap";
 
 import ContentContainer from "../content-container/ContentContainer";
+import ProjectCard from "./ProjectCard";
 
 class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectData: [],
+    };
+    this.projectAPI =
+      "https://d90832a0.us-south.apigw.appdomain.cloud/get-projects/";
+  }
+
+  componentDidMount() {
+    (async () => {
+      const rawResponse = await fetch(this.projectAPI, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.state.form),
+      });
+      const res = await rawResponse.json();
+      this.setState({ projectData: res.projects });
+    })();
+  }
   render() {
     return (
       <div>
-        <ContentContainer style={{ textAlign: "center", border: "0" }}>
-          <div>
-            {projectData.map((x, i) => {
-              return (
-                <ProjectCard
-                  media={x.projectMedia}
-                  title={i}
-                  text={x.projectDesc}
-                  link={x.projectLink}
-                  key={i}
-                />
-              );
-            })}
-          </div>
+        <ContentContainer style={{ textAlign: "center" }}>
+          {this.state.projectData.map((x, i) => {
+            return (
+              <ProjectCard
+                media={x.projectMedia}
+                title={x.projectName}
+                text={x.projectDesc}
+                link={x.projectLink}
+                key={i}
+              />
+            );
+          })}
         </ContentContainer>
       </div>
     );
   }
 }
-
-function ProjectCard(props) {
-  const cardStyles = {
-    width: "400px",
-    height: "400px",
-    verticalAlign: "top",
-    display: "inline-block",
-    margin: "0 auto",
-    marginLeft: "30px",
-    marginTop: "30px",
-  };
-  return (
-    <Card style={cardStyles}>
-      <Card.Img
-        src={props.media}
-        style={{ width: "inherit", height: "200px" }}
-      />
-      <Card.Body>
-        <Card.Title>{"project " + props.title}</Card.Title>
-        <Card.Text>{props.text}</Card.Text>
-        <Card.Link href={props.link}>Get to know more</Card.Link>
-      </Card.Body>
-    </Card>
-  );
-}
-
-const projectData = Array(5).fill({
-  projectName: "project1",
-  projectDesc:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla magna dolor, tincidunt at urna ultricies, commodo tristique dolor. Nulla mollis, arcu eget fermentum egestas, mauris",
-  projectMedia:
-    "https://github.com/whpskg/covid-19-SafeDistancing/blob/master/assets/app-demo.gif?raw=true",
-  projectLink: "https://github.com/",
-});
 
 export default Projects;
